a88ef35d39852f599f8bf6c5a8f462330c9f2a12
Ahmad Effati <ahmad.efati@gmail.com>
2021-03-09 09:37:12 +0330

    remove generated build files

--- base/build/tmp/kapt3/stubs/debug/com/androiddevs/base/TypealiasKt.java
+++
@@ -1,7 +0,0 @@
-package com.androiddevs.base;
-
-import com.google.android.gms.maps.model.LatLng;
-
-@kotlin.Metadata(mv = {1, 4, 0}, bv = {1, 0, 3}, k = 2, d1 = {"\u0000\u0012\n\u0000\n\u0002\u0010!\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000*\u0016\u0010\u0000\"\b\u0012\u0004\u0012\u00020\u00020\u00012\b\u0012\u0004\u0012\u00020\u00020\u0001* \u0010\u0003\"\b\u0012\u0004\u0012\u0002`\u00040\u00012\u0012\u0012\u000e\u0012\f\u0012\u0004\u0012\u00020\u00020\u0001j\u0002`\u00040\u0001\u00a8\u0006\u0005"}, d2 = {"Polyline", "", "Lcom/google/android/gms/maps/model/LatLng;", "Polylines", "Lcom/androiddevs/base/Polyline;", "base_debug"})
-public final class TypealiasKt {
-}
